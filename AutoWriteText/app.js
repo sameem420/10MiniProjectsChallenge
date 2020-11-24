62b409f8e0627526826cbe09b60b5d7d15404aad
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-11-24 21:11:29 +0500

    ✔️ Auto-Text Writer done ...

--- AutoWriteText/app.js
+++ AutoWriteText/app.js
@@ -1,1 +1,15 @@
-document.onload();
+const myText = 'I am React/React Native Engineer';
+const divWrapper = document.getElementById('wrapper');
+
+let index = 0;
+
+writeText = () => {
+    divWrapper.innerText = myText.slice(0,index);
+    index++;
+    if(index > myText.length)
+    {
+        index = 0;
+    }
+}
+
+setInterval(writeText, 100);
