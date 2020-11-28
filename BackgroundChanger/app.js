f1e9c14337503e459f059267676a847bdcf91f02
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-11-28 22:25:56 +0500

    :white_check_mark:  background changer done ,,,

--- BackgroundChanger/app.js
+++ BackgroundChanger/app.js
@@ -1,1 +1,9 @@
-document.onload();
+const btnBGChanger = document.getElementById('bgBTN');
+
+btnBGChanger.addEventListener('click', () => {
+    document.body.style.background = randomBG();
+})
+
+const randomBG= () => {
+    return `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
+}
