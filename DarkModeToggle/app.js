62aa10a132ffeb490f919684b0b0708ebb0838a4
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-11-28 22:58:47 +0500

    :white_check_mark:  DarkMode Toggler done ...

--- DarkModeToggle/app.js
+++ DarkModeToggle/app.js
@@ -1,1 +1,5 @@
-document.onload();
+const toggler = document.getElementById('toggle');
+
+toggler.addEventListener('change', (e) => {
+    document.body.classList.toggle('dark', e.target.checked);
+})
