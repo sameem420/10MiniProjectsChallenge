afd8f723dd0ec816232eb520c9584e80d4a5b462
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-11-30 22:45:07 +0500

    ✔️ Image Carousel done ...

--- ImageCarousel/app.js
+++ ImageCarousel/app.js
@@ -1,1 +1,17 @@
-document.onload();
+const imgs = document.getElementById("imgs");
+
+const img = document.querySelectorAll("#imgs img");
+
+let idx = 0;
+
+function runCarousel() {
+ idx++;
+ if(idx > img.length - 1)
+ {
+     idx = 0;
+ }
+ imgs.style.transform = `translateX(${-idx * 500}px)`;
+ 
+}
+
+setInterval(runCarousel, 2000);
