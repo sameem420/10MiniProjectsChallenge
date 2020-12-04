a1451d2762a3d08908f32eea87600771bc2660b4
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-12-04 20:51:32 +0500

    ✔️ Image ZoomEffect done ...

--- ZoomEffect/app.js
+++ ZoomEffect/app.js
@@ -1,1 +1,15 @@
-document.onload();
+const container = document.getElementById("container");
+const img = document.querySelector("img");
+
+container.addEventListener('mousemove', (e) => {
+    const x = e.clientX -  e.target.offsetLeft;
+    const y = e.clientY -  e.target.offsetTop;
+
+    img.style.transformOrigin = `${x}px ${y}px`;
+    img.style.transform = `scale(2)`;
+});
+
+container.addEventListener('mouseleave', () => {
+    img.style.transformOrigin = `center center`;
+    img.style.transform = `scale(1)`;
+});
