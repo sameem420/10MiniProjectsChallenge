e9b01c78fec4069846b86356c1af17d298538662
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-12-04 20:35:16 +0500

    ✔️ Sound board done ...

--- SoundBoard/app.js
+++ SoundBoard/app.js
@@ -1,1 +1,20 @@
-document.onload();
+const sounds = ["DirilisMusic1","DirilisMusic2","DirilisMusic3","DirilisMusic4"];
+
+sounds.forEach((sound) => {
+    const btn = document.createElement("button");
+    btn.innerText = sound;
+    btn.classList.add('btn');
+    document.body.appendChild(btn);
+    btn.addEventListener("click", () => {
+        stopSong();
+        document.getElementById(sound).play();
+    });
+});
+
+const stopSong = () => {
+    sounds.forEach((sound) => {
+        const currentSong = document.getElementById(sound);
+        currentSong.pause();
+        currentSong.currentTime = 0;
+    });
+}
