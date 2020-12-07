96e12e55d9a83d19f6674e96f34e5392b37b74a1
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-12-07 23:51:16 +0500

    :heavy_check_mark:  Added all projects

--- Hamburger/app.js
+++ Hamburger/app.js
@@ -1,1 +1,6 @@
-document.onload();
+let navWrapper = document.querySelector('.nav-wrapper'),
+navToogler =  document.querySelector('.nav-toogler')
+
+navToogler.addEventListener('click', function (event) {
+navWrapper.classList.toggle('active')
+})
